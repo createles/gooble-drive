71b9299de500ddb2945480ec23f54e876ded699b
createlsworklappy <justinecastillo7@gmail.com>
2026-04-14 15:53:26 +0900

    refactor: remove dashboardRouter file due to redundancy

--- routes/dashboardRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { isAuth } from "../middleware/authMiddleware.js"; // Updated import path
-import { getDashboard } from "../controllers/dashboardController.js";
-
-const dboardRouter = Router();
-
-dboardRouter.get('/', isAuth, getDashboard);
-
-export default dboardRouter;
