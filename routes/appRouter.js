273eeaaccf4a44c2931c4d0f9dece18e2e4cfd82
createlsworklappy <justinecastillo7@gmail.com>
2026-04-27 13:30:15 +0900

    feat: hook delete routes to app

--- routes/appRouter.js
+++ routes/appRouter.js
@@ -4,7 +4,7 @@ import { isAuth, isLoggedIn } from "../middleware/authMiddleware.js"; // Updated
 import { getDashboard, postCreateFolder } from "../controllers/dashboardController.js";
 import { handleUpload, upload } from "../controllers/uploadController.js";
 import dashboardRouter from "./dashboardRouter.js";
-import { generateShareLink, getFileMetadata, startDownload } from "../controllers/fileController.js";
+import { deleteFile, deleteFolder, generateShareLink, getFileMetadata, startDownload } from "../controllers/fileController.js";
 
 const appRouter = Router();
 
@@ -27,5 +27,7 @@ appRouter.post('/folders/create', isAuth, postCreateFolder);
 // Route for Fetch API call to get shareable link
 appRouter.post('/files/share', isAuth, generateShareLink);
 
+appRouter.delete('/files/:fileId', isAuth, deleteFile);
+appRouter.delete('/folders/:folderId', isAuth, deleteFolder);
 
 export default appRouter;
